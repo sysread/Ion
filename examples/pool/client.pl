3911af3999cb12fe5f531a219e4a76627c1c4737
Jeff Ober <sysread@fastmail.fm>
2018-07-20 19:51:03 +0000

    Pool client now reads results asynchronously

--- examples/pool/client.pl
+++ examples/pool/client.pl
@@ -2,6 +2,7 @@
 
 use feature 'say';
 use common::sense;
+use Coro;
 use Data::Dump::Streamer;
 use MIME::Base64 qw(encode_base64 decode_base64);
 use Ion;
@@ -23,8 +24,15 @@ $client
   >> sub{ Dump(shift)->Purity(1)->Declare(1)->Indent(0)->Out }
   >> sub{ encode_base64($_[0], '') };
 
+my @pending;
+
 foreach my $i (1 .. $count) {
-  $client->([\&work, $i]);
-  my $result = <$client>;
-  say $i, ' * 2 = ', $result;
+  push @pending, async{
+    my $i = shift;
+    $client->([\&work, $i]);
+    my $result = <$client>;
+    say $i, ' * 2 = ', $result;
+  } $i;
 }
+
+$_->join foreach @pending;
